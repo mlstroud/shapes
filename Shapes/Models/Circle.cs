ecd9c504d203b9d2a6cc1190aee77edf4965e785
Matt Stroud <lixingke06@gmail.com>
2020-05-13 09:30:22 -0700

    add code to return circumference

--- Shapes/Models/Circle.cs
+++ Shapes/Models/Circle.cs
@@ -1,3 +1,4 @@
+using System;
 namespace Shapes.Models
 {
   public class Circle
@@ -12,6 +13,11 @@ namespace Shapes.Models
     public int GetDiameter()
     {
       return Radius * 2;
+    }
+
+    public double GetCircumference()
+    {
+      return Math.Round((Radius * 2) * Math.PI, 2);
     }
   }
 }
