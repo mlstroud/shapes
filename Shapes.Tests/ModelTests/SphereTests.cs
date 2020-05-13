d115eb82216a85ff2e89de51ed785faa6b0f5fcd
Matt Stroud <lixingke06@gmail.com>
2020-05-13 09:43:11 -0700

    add test for GetSurfaceArea

--- Shapes.Tests/ModelTests/SphereTests.cs
+++ Shapes.Tests/ModelTests/SphereTests.cs
@@ -12,9 +12,20 @@ namespace Shapes.TestTools
       Circle newCircle = new Circle(5);
       Sphere newSphere = new Sphere(newCircle);
 
-      int result = newSphere.GetVolume();
+      double result = newSphere.GetVolume();
 
       Assert.AreEqual(result, 523.60);
+    }
+
+    [TestMethod]
+    public void Sphere_GetSurfaceArea_SurfaceAreaOfSphere()
+    {
+      Circle newCircle = new Circle(5);
+      Sphere newSphere = new Sphere(newCircle);
+
+      double result = newSphere.GetSurfaceArea();
+
+      Assert.AreEqual(result, 314.16);
     }
   }
 }
