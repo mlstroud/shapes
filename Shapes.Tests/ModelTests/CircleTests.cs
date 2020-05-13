ae8f7920eb04fe6ab73528b2e830b3d1cc88c8b4
Matt Stroud <lixingke06@gmail.com>
2020-05-13 09:25:22 -0700

    add test for getting circle circumference

--- Shapes.Tests/ModelTests/CircleTests.cs
+++ Shapes.Tests/ModelTests/CircleTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using Shapes.Models;
 
 namespace Shapes.TestTools
@@ -14,6 +15,16 @@ namespace Shapes.TestTools
       int result = newCircle.GetDiameter();
 
       Assert.AreEqual(result, 10);
+    }
+
+    [TestMethod]
+    public void Circle_GetCircumference_CircumferenceOfCircle()
+    {
+      Circle newCircle = new Circle(5);
+
+      Decimal result = newCircle.GetCircumference();
+
+      Assert.AreEqual(result, 31.42);
     }
   }
 }
