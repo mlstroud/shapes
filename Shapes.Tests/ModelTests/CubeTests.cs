db09f7dfdce266f7c7f6e69834c1be09a13a8027
Matt Stroud <lixingke06@gmail.com>
2020-05-13 09:15:18 -0700

    add test for surface area of cube

--- Shapes.Tests/ModelTests/CubeTests.cs
+++ Shapes.Tests/ModelTests/CubeTests.cs
@@ -16,5 +16,16 @@ namespace Shapes.Tests
 
       Assert.AreEqual(result, 64);
     }
+
+    [TestMethod]
+    public void Cube_GetSurfaceArea_SurfaceAreaOfCube()
+    {
+      Rectangle newRectangle = new Rectangle(4, 4);
+      Cube newCube = new Cube(newRectangle);
+
+      int result = newCube.GetSurfaceArea();
+
+      Assert.AreEqual(result, 96);
+    }
   }
 }
